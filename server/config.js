7a0370cd81ced788d0f1a1c39e7d85bc52f2e52e
Vishal Gupta <vishalnph@gmail.com>
2025-02-01 19:41:51 +0530

    update git

--- server/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGODB_URL: "mongodb+srv://vishal:D77wNVXoao2ZhJr8@cluster0.6ixar.mongodb.net/amazona?retryWrites=true&w=majority&appName=Cluster0",
-    JWT_SECRET: "sldknf2j3423on3nj4noiodsoi23joijo3i2j42"
-}
